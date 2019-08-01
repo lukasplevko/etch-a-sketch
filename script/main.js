3b26746fe5c13d04c059a3c6c7448391884e4687
lukasplevko <lukas.plevk01@gmail.com>
2019-08-01 22:41:44 +0200

    commit for the peeps who are willing to help me solve this

--- script/main.js
+++ script/main.js
@@ -1,26 +1,35 @@
 const container = document.querySelector(".grid-container");
+const btn = document.getElementById("btn");
+btn.addEventListener("click", resetGrid)
+let gridSize = prompt();
+
 
    
     function createGrid(x)
     {
-        for(let i = 0; i < x; i++){
-        
-             
+
+        for(let i = 0; i < x; i++){   
             for(let j = 0; j <x; j++){
                 let div = document.createElement("div");
                 div.classList.add("grid-item");
-                container.appendChild(div); 
-                
-            }
-            
+                container.appendChild(div);
+                div.addEventListener("mouseover", colorGrid)    
+          }     
         }
         container.setAttribute("style",`grid-template-columns:  repeat(${x}, 1fr);`)
+    } 
+ 
+    function colorGrid(){
+       this.setAttribute("style","background:pink");
+
     }
-    
-         
-        createGrid(16);
+
+        createGrid(gridSize);
       
     
+    function resetGrid(){
+        location.reload("index.html");
+    }
 
     
        
